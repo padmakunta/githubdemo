efe20352c33e6b0680269d95ead1a903019866c9
kunta_000 <kunta_000@192.168.113.192>
2017-09-20 11:01:00 -0500

    first commit

---
+++ src/githubdemo/githubdemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubdemo;
+
+/**
+ *
+ * @author kunta_000
+ */
+public class githubdemo {
+//psvm
+    public static void main(String[] args){
+     // sout
+     System.out.println("Hello World");
+    
+}
+    
+}
